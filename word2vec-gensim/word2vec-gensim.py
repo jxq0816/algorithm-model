5cb4e07129c6084f11a8a1cf88b08823ae132cf1
姜兴琪 <xingqijiang@qq.com>
2021-08-01 20:14:24 +0800

    word2vec

--- word2vec-gensim/word2vec-gensim.py
+++ word2vec-gensim/word2vec-gensim.py
@@ -2,11 +2,10 @@ from gensim.test.utils import common_texts,get_tmpfile
 from gensim.models import Word2Vec
 print(common_texts)
 '''
-[['human', 'interface', 'computer'], 
-['survey', 'user', 'computer', 'system', 'response', 'time'], 
+[['human', 'interface', 'computer'], ['survey', 'user', 'computer', 'system', 'response', 'time'], 
 ['eps', 'user', 'interface', 'system'], ['system', 'human', 'system', 'eps'], 
-['user', 'response', 'time'], ['trees'], 
-['graph', 'trees'], ['graph', 'minors', 'trees'], ['graph', 'minors', 'survey']]
+['user', 'response', 'time'], ['trees'], ['graph', 'trees'], 
+['graph', 'minors', 'trees'], ['graph', 'minors', 'survey']]
 '''
 model = Word2Vec(common_texts,vector_size=2, window=5, min_count=1, workers=4)
 print(model.wv['computer'])
