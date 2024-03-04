9a4ccb596a086152650014e8cac224aee6364509
xingqi5 <xingqi5@staff.sina.com>
2024-03-04 19:01:38 +0800

    bert

--- bert/bert_text_classification.py
+++ bert/bert_text_classification.py
@@ -2,10 +2,12 @@ from transformers import pipeline
 
 classifier = pipeline("text-classification",model='bhadresh-savani/bert-base-uncased-emotion', top_k=1)
 
-prediction = classifier("开心")
+print(classifier("开心"))
 
-print(prediction)
+print(classifier("happy"))
 
-prediction = classifier("生气")
+print(classifier("have a nice day"))
 
-print(prediction)
+print(classifier("我今天很生气"))
+
+print(classifier("angry"))
