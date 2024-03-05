74755bf4e3064f9e044d70f66a61a7932e3e184f
xingqi5 <xingqi5@staff.sina.com>
2024-03-05 12:34:55 +0800

    cemotion

--- cemotion/cemotion_demo.py
+++ cemotion/cemotion_demo.py
@@ -1,9 +1,23 @@
-#按文本字符串分析
 from cemotion import Cemotion
 
-str_text1 = '配置顶级，不解释，手机需要的各个方面都很完美'
-str_text2 = '院线看电影这么多年以来，这是我第一次看电影睡着了。简直是史上最大烂片！没有之一！侮辱智商！大家小心警惕！千万不要上当！再也不要看了！'
 
-c = Cemotion()
-print('"', str_text1 , '"\n' , '预测值:{:6f}'.format(c.predict(str_text1) ) , '\n')
-print('"', str_text2 , '"\n' , '预测值:{:6f}'.format(c.predict(str_text2) ) , '\n')
+def text_emotion(text, c):
+
+    val = c.predict(text)
+    #print('预测值:{:6f}'.format(val))
+    if val > 0.5:
+        print('"', text, '"\n', '情感分类：正向', '\n')
+    else:
+        print('"', text, '"\n', '情感分类：负向', '\n')
+
+
+if __name__ == "__main__":
+
+    text1 = '刘亦菲的气质像一个仙女下凡'
+    text2 = '今天下班被追尾了，生气'
+    text3 = '今天下雪了欸，好开心呀'
+
+    c = Cemotion()
+    text_emotion(text1,c)
+    text_emotion(text2,c)
+    text_emotion(text3,c)
