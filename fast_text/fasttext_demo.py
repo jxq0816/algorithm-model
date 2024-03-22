0b9156fe66f90895e333807b8bf38ca2f68fc365
xingqi5 <xingqi5@staff.sina.com>
2024-03-22 18:16:16 +0800

    fasttext

--- fast_text/fasttext_demo.py
+++ fast_text/fasttext_demo.py
@@ -1,15 +1,42 @@
+import os
 import fasttext
+from sklearn.metrics import classification_report
 
-# 训练模型
-model = fasttext.train_supervised('train.txt')  # 'train.txt'是你的训练数据文件，需要包含标签和文本
 
-# 测试模型
-result = model.test('test.txt')  # 'test.txt'是你的测试数据文件，只需要包含文本
-print('Precision:', result[1])
-print('Recall:', result[2])
+def save_model(model):
+    model.save_model("fasttext.bin")
 
-# 对单个文本进行预测
-input_text = "This is a sample text"
-labels, probabilities = model.predict(input_text, k=1)  # k表示返回的最可能的标签数量
-print('Predicted label:', labels[0])
-print('Probability:', probabilities[0])
+
+def load_model():
+    return fasttext.load_model("fasttext.bin")
+
+
+def evaluate_model(test_file):
+    inputs = []
+    y_true = []
+    for line in open(test_file,encoding='utf-8'):
+        if line.strip() == "":
+            continue
+        tmp = line.strip().split("\t")
+        label = tmp[0]
+        text = "\t".join(tmp[1:])
+        y_true.append(label)
+        inputs.append(text)
+    model = load_model()
+    y_pred = model.predict(inputs)[0]
+    report = classification_report(y_true, y_pred)
+    return report
+
+
+if __name__ == "__main__":
+
+    current_path = os.getcwd()
+    parent_path = os.path.dirname(current_path)
+    train_filepath = os.path.join(parent_path,'0_data_set','imdb','train.txt')
+    test_filepath = os.path.join(parent_path, '0_data_set', 'imdb', 'test.txt')
+    #model = fasttext.train_supervised(train_filepath, epoch=50, lr=0.05, dim=300, maxn=128)
+    #save_model(model)
+    report = evaluate_model(test_filepath)
+    print(report)
+    with open("fasttext_exp.log", "w+") as fp:
+        fp.write(report)
